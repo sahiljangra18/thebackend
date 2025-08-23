5f4c51115ec78048205a49ec96c01172f3a9a940
sahiljangra18 <jangrasahil2021@gmail.com>
2025-08-23 14:59:40 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+    }
+})()
+*/
